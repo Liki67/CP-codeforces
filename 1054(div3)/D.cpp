df53595e5275297bd43f2043a002a502d3091ad7
Liki67 <likith297@gmail.com>
2025-10-08 21:06:55 +0530

    jj

--- 1054(div3)/D.cpp
+++ 1054(div3)/D.cpp
@@ -2,9 +2,26 @@
 #include<vector>
 #include<string>
 using namespace std;
+int get(vector<int>&a){
+    if(a.empty())return 0;
+    int pos=a.size()/2,val=0;
+    for(int i=0;i<a.size();i++){
+        val+=abs(a[pos]-a[i])-abs(pos-i);
+    }
+    cout<<val<<a[0]<<endl;
+    return val;
+}
 void solve(){
-    int n;
-    cin>>n;
+    int n;cin>>n;
+    vector<int>a,b;
+    for(int i=0;i<n;i++){
+        char c;
+        cin>>c;
+        if(c == 'a')a.push_back(i);
+        else b.push_back(i);
+    }
+    cout<<min(get(a),get(b))<<endl;
+
 }
 int main(int argc, char const *argv[])
 {
